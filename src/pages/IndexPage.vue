e73d424abc4a5844d530135558b8c9e6d8128be2
reda awad <redaawad622@gmail.com>
2022-06-17 14:24:43 +0200

    first commit

---
+++ src/pages/IndexPage.vue
@@ -0,0 +1,25 @@
+<template>
+  <q-page class="flex flex-center">
+    <q-circular-progress
+      :value="20"
+      size="45px"
+      :thickness="1"
+      color="grey-8"
+      track-color="orange"
+      class="q-ma-md"
+    />
+    <img
+      alt="Quasar logo"
+      src="~assets/quasar-logo-vertical.svg"
+      style="width: 200px; height: 200px"
+    />
+  </q-page>
+</template>
+
+<script>
+import { defineComponent } from "vue";
+
+export default defineComponent({
+  name: "IndexPage",
+});
+</script>
